06b0732f301f686275a2e221491edc00bf4badad
Alex <git@pentive.com>
2021-12-12 13:36:35 -0600

    pnpm i -D eslint typescript @typescript-eslint/parser @typescript-eslint/eslint-plugin
    https://github.com/typescript-eslint/typescript-eslint/blob/HEAD/docs/linting/README.md
    
    eslint --init

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+  },
+}
