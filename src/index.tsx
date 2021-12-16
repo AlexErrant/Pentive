2eba1ca4943cadc68c0ee760c1b20a56a8e57c28
Alex <git@pentive.com>
2021-12-15 18:27:49 -0600

    pnpm i solid-app-router

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,13 @@
-import { render } from "solid-js/web"
-
 import "./index.css"
+import { render } from "solid-js/web"
+import { Router } from "solid-app-router"
 import App from "./App"
 
-render(() => <App />, document.getElementById("root") as HTMLElement)
+render(
+  () => (
+    <Router>
+      <App />
+    </Router>
+  ),
+  document.getElementById("root") as HTMLElement
+)
