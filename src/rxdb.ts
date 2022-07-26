a0fe60d6c7bc26bf7fe7f4b163c429652d7363b1
Alex <git@pentive.com>
2022-07-26 18:06:09 -0500

    added 2nd half as demoFunction

--- src/rxdb.ts
+++ src/rxdb.ts
@@ -29,3 +29,97 @@ type MyDatabaseCollections = {
 }
 
 type MyDatabase = RxDatabase<MyDatabaseCollections>
+
+export async function demoFunction(): Promise<void> {
+  /**
+   * create database and collections
+   */
+  const myDatabase: MyDatabase = await createRxDatabase<MyDatabaseCollections>({
+    name: "mydb",
+    storage: getRxStoragePouch("memory"),
+  })
+
+  const heroSchema: RxJsonSchema<HeroDocType> = {
+    title: "human schema",
+    description: "describes a human being",
+    version: 0,
+    keyCompression: true,
+    primaryKey: "passportId",
+    type: "object",
+    properties: {
+      passportId: {
+        type: "string",
+      },
+      firstName: {
+        type: "string",
+      },
+      lastName: {
+        type: "string",
+      },
+      age: {
+        type: "integer",
+      },
+    },
+    required: ["passportId", "firstName", "lastName"],
+  }
+
+  const heroDocMethods: HeroDocMethods = {
+    scream: function (this: HeroDocument, what: string) {
+      return this.firstName + " screams: " + what.toUpperCase()
+    },
+  }
+
+  const heroCollectionMethods: HeroCollectionMethods = {
+    countAllDocuments: async function (this: HeroCollection) {
+      const allDocs = await this.find().exec()
+      return allDocs.length
+    },
+  }
+
+  await myDatabase.addCollections({
+    heroes: {
+      schema: heroSchema,
+      methods: heroDocMethods,
+      statics: heroCollectionMethods,
+    },
+  })
+
+  // add a postInsert-hook
+  myDatabase.heroes.postInsert(
+    function myPostInsertHook(
+      this: HeroCollection, // own collection is bound to the scope
+      docData: HeroDocType, // documents data
+      doc: HeroDocument // RxDocument
+    ) {
+      console.log("insert to " + this.name + "-collection: " + doc.firstName)
+    },
+    false // not async
+  )
+
+  /**
+   * use the database
+   */
+
+  // insert a document
+  const hero: HeroDocument = await myDatabase.heroes.insert({
+    passportId: "myId",
+    firstName: "piotr",
+    lastName: "potter",
+    age: 5,
+  })
+
+  // access a property
+  console.log(hero.firstName)
+
+  // use a orm method
+  hero.scream("AAH!")
+
+  // use a static orm method from the collection
+  const amount: number = await myDatabase.heroes.countAllDocuments()
+  console.log(amount)
+
+  /**
+   * clean up
+   */
+  myDatabase.destroy()
+}
