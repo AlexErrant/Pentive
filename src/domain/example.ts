8c930d2809f2d8f1e88f56f0b9dcee4a0dbc6892
Alex <git@pentive.com>
2022-08-03 17:51:00 -0500

    added sampleExample

--- src/domain/example.ts
+++ src/domain/example.ts
@@ -70,3 +70,53 @@ export interface Example {
   created: Date
   modified: Date
 }
+
+export const sampleExample = {
+  id: "B598A95F-2372-45DE-B7A6-29CA67A10D8E" as ExampleId,
+  authorId: "FA12DB13-7DA3-4CA1-8C46-86379CC34232" as UserId,
+  frontPersonalField: "",
+  backPersonalField: "",
+  deckIds: new Set(),
+  tags: new Set(),
+  cards: [
+    {
+      pointer: {
+        tag: "Normal",
+        id: "ED061BC3-B183-4C55-BE0D-0A820F491CE1" as CardTemplateId,
+      },
+      cardSettingId: "D85F630A-EAC4-41DD-B7AA-5F6DEF5FC4FA" as CardSettingId,
+      easeFactor: 0,
+      intervalOrStepsIndex: {
+        tag: "NewStepsIndex",
+        index: 0,
+      },
+      due: new Date(),
+      isLapsed: false,
+      reviews: [],
+      state: "Normal",
+    },
+    {
+      pointer: {
+        tag: "Normal",
+        id: "ED061BC3-B183-4C55-BE0D-0A820F491CE1" as CardTemplateId, // should be different from other card
+      },
+      cardSettingId: "D85F630A-EAC4-41DD-B7AA-5F6DEF5FC4FA" as CardSettingId,
+      easeFactor: 0,
+      intervalOrStepsIndex: {
+        tag: "NewStepsIndex",
+        index: 0,
+      },
+      due: new Date(),
+      isLapsed: false,
+      reviews: [],
+      state: "Normal",
+    },
+  ],
+  specimenRevisionId: null,
+  ankiNoteId: null,
+  title: null,
+  templateId: "EC2EFBBE-C944-478A-BFC4-023968B38A72" as TemplateId,
+  fieldValues: new Map(),
+  created: new Date(),
+  modified: new Date(),
+}
