a50f986f4610382d55d689c559f17425faf1615d
Alex <git@pentive.com>
2022-08-16 07:10:02 -0500

    fixed some casing

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,11 @@
-import type { Component } from "solid-js"
+import type { JSX } from "solid-js"
 import { NavLink, useRoutes, useLocation } from "solid-app-router"
 
 import { routes } from "./routes"
 
-const App: Component = () => {
+export default function App(): JSX.Element {
   const location = useLocation()
+  // eslint-disable-next-line @typescript-eslint/naming-convention
   const Route = useRoutes(routes)
 
   return (
@@ -58,5 +59,3 @@ const App: Component = () => {
     </>
   )
 }
-
-export default App
