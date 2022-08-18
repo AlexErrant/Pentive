25f91026a1a583e0fe2a44cb0d9a2c7fd3dec10a
Alex <git@pentive.com>
2022-08-18 14:41:41 -0500

    copied code from the example on https://www.npmjs.com/package/solid-element
    https://webcomponents.dev/edit/yGRb00wV4AMncPupRmA9

--- example-plugins/solid-web-component/src/index.tsx
+++ example-plugins/solid-web-component/src/index.tsx
@@ -1,6 +1,35 @@
-/* @refresh reload */
-import { render } from 'solid-js/web';
+// todo - investigate a less trash build setup. https://www.reddit.com/r/solidjs/comments/gym0ed/bundling_a_web_components/
 
-import App from './App';
+import { createSignal } from "solid-js";
+import { customElement } from "solid-element";
 
-render(() => <App />, document.getElementById('root') as HTMLElement);
+const style = `div * {
+          font-size: 200%;
+        }
+
+        span {
+          width: 4rem;
+          display: inline-block;
+          text-align: center;
+        }
+
+        button {
+          width: 4rem;
+          height: 4rem;
+          border: none;
+          border-radius: 10px;
+          background-color: seagreen;
+          color: white;
+        }`;
+
+customElement("pentive-nav", () => {
+  const [count, setCount] = createSignal(0);
+  return (
+    <div>
+      <style>{style}</style>
+      <button onClick={() => setCount(count() - 1)}>-</button>
+      <span>{count}</span>
+      <button onClick={() => setCount(count() + 1)}>+</button>
+    </div>
+  );
+});
