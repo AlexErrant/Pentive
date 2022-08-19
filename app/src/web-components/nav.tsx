efd36c4c875f37786aaddcfdc052b7be0e758785
Alex <git@pentive.com>
2022-08-18 19:35:58 -0500

    made nav data driven

--- app/src/web-components/nav.tsx
+++ app/src/web-components/nav.tsx
@@ -1,39 +1,58 @@
-import type { JSX } from "solid-js"
+import { For, JSX } from "solid-js"
 import { NavLink, useLocation } from "solid-app-router"
+
+interface NavLinkData {
+  content: JSX.Element
+  href: string
+  className: string
+  activeClass: string
+  end: boolean
+}
+
+const defaultNavLink = {
+  className: "no-underline hover:underline",
+  activeClass: "font-bold",
+  end: false,
+}
+
+const navLinks: NavLinkData[] = [
+  {
+    ...defaultNavLink,
+    content: "Home",
+    href: "/",
+    end: true,
+  },
+  {
+    ...defaultNavLink,
+    content: "About",
+    href: "/about",
+  },
+  {
+    ...defaultNavLink,
+    content: "Error",
+    href: "/error",
+  },
+]
 
 export default function Nav(): JSX.Element {
   const location = useLocation()
   return (
     <nav class="bg-gray-200 text-gray-900 px-4">
       <ul class="flex items-center">
-        <li class="py-2 px-4">
-          <NavLink
-            href="/"
-            class="no-underline hover:underline"
-            activeClass="font-bold"
-            end
-          >
-            Home
-          </NavLink>
-        </li>
-        <li class="py-2 px-4">
-          <NavLink
-            href="/about"
-            class="no-underline hover:underline"
-            activeClass="font-bold"
-          >
-            About
-          </NavLink>
-        </li>
-        <li class="py-2 px-4">
-          <NavLink
-            href="/error"
-            class="no-underline hover:underline"
-            activeClass="font-bold"
-          >
-            Error
-          </NavLink>
-        </li>
+        <For each={navLinks}>
+          {({ href, className, activeClass, end, content }) => (
+            <li class="py-2 px-4">
+              <NavLink
+                href={href}
+                class={className}
+                activeClass={activeClass}
+                end={end}
+              >
+                {content}
+              </NavLink>
+            </li>
+          )}
+        </For>
 
         <li class="text-sm flex items-center space-x-1 ml-auto">
           <span>URL:</span>
