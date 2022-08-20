824393658c580eb895603b084a9c0327ae282c31
Alex <git@pentive.com>
2022-08-20 08:58:07 -0500

    Added basic table to /template
    https://tanstack.com/table/v8/docs/examples/solid/basic

--- app/src/pages/templates.tsx
+++ app/src/pages/templates.tsx
@@ -1,6 +1,49 @@
-import { JSX, createEffect, Suspense } from "solid-js"
+import { JSX, createEffect, Suspense, For } from "solid-js"
 import { useRouteData } from "solid-app-router"
 import TemplatesData from "./templates.data"
+import {
+  ColumnDef,
+  createSolidTable,
+  flexRender,
+  getCoreRowModel,
+} from "@tanstack/solid-table"
+import { Template } from "../domain/template"
+
+function id(id: keyof Template): keyof Template {
+  return id
+}
+
+function remoteCell(hasRemote: boolean): JSX.Element {
+  return hasRemote ? "☁" : ""
+}
+
+const columns: Array<ColumnDef<Template>> = [
+  {
+    header: "Name",
+    accessorKey: id("name"),
+  },
+  {
+    header: "Type",
+    accessorFn: (row) => row.templateType.tag,
+  },
+  {
+    header: "Remote",
+    accessorFn: (row) => row.sourceId != null,
+    cell: (info) => remoteCell(info.getValue<boolean>()),
+  },
+  {
+    header: "Fields",
+    accessorFn: (row) => row.fields.map((f) => f.name).join(", "),
+  },
+  {
+    header: "Created",
+    accessorKey: id("created"),
+  },
+  {
+    header: "Modified",
+    accessorKey: id("modified"),
+  },
+]
 
 export default function Templates(): JSX.Element {
   const templates = useRouteData<typeof TemplatesData>()
@@ -9,18 +52,62 @@ export default function Templates(): JSX.Element {
     console.log(templates())
   })
 
+  const table = createSolidTable({
+    get data() {
+      return templates()
+    },
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  })
+
   return (
     <section class="bg-pink-100 text-gray-700 p-8">
       <h1 class="text-2xl font-bold">Templates</h1>
 
       <p class="mt-4">A page all about this website.</p>
 
-      <p>
-        <span>We love</span>
-        <Suspense fallback={<span>...</span>}>
-          <span>&nbsp;{templates().length}</span>
-        </Suspense>
-      </p>
+      <Suspense fallback={<span>Loading...</span>}>
+        <table>
+          <thead>
+            <For each={table.getHeaderGroups()}>
+              {(headerGroup) => (
+                <tr>
+                  <For each={headerGroup.headers}>
+                    {(header) => (
+                      <th>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                      </th>
+                    )}
+                  </For>
+                </tr>
+              )}
+            </For>
+          </thead>
+          <tbody>
+            <For each={table.getRowModel().rows}>
+              {(row) => (
+                <tr>
+                  <For each={row.getVisibleCells()}>
+                    {(cell) => (
+                      <td>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </td>
+                    )}
+                  </For>
+                </tr>
+              )}
+            </For>
+          </tbody>
+        </table>
+      </Suspense>
     </section>
   )
 }
