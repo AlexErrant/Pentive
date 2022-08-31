2c44279e8c43538b3c648b2eb1dabe9b8d1132cf
Alex <git@pentive.com>
2022-08-31 15:40:50 -0500

    added some learnings regarding svelte's custom-element/web-component support

--- example-plugins/svelte-custom-element/src/App.svelte
+++ example-plugins/svelte-custom-element/src/App.svelte
@@ -1,3 +1,9 @@
+<!--
+  Reference: https://svelte.dev/docs#run-time-custom-element-api
+  There's no way to disable the Shadow DOM: https://github.com/sveltejs/svelte/issues/1748
+  lowTODO improve the build https://www.thisdot.co/blog/web-components-with-svelte
+ -->
+
 <svelte:options tag="pentive-nav" />
 
 <script lang="ts">
