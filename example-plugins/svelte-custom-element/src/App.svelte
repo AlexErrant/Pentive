8684b69ab998dca7fef16651266c7a88c0ec4d0d
Alex <git@pentive.com>
2022-08-31 10:40:50 -0500

    made svelte-custom-element implement nav

--- example-plugins/svelte-custom-element/src/App.svelte
+++ example-plugins/svelte-custom-element/src/App.svelte
@@ -1,45 +1,25 @@
+<svelte:options tag="pentive-nav" />
+
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import type { NavLinkData } from "../../../app/src/custom-elements/contracts";
+  export let navLinks: NavLinkData[] = [];
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+<nav>
+  <ul>
+    {#each navLinks as {name, href}}
+      <li>
+        <a href={href}>
+          {name}
+        </a>
+      </li>
+    {/each}
+  </ul>
+</nav>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  a {
+    background: orange;
+    color: white;
   }
 </style>
