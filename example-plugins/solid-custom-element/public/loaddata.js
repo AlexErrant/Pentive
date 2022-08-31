c1d976604f1ca16b85d558897e58914fde96879e
Alex <git@pentive.com>
2022-08-31 07:26:32 -0500

    added loaddata to solid

---
+++ example-plugins/solid-custom-element/public/loaddata.js
@@ -0,0 +1,23 @@
+let myNav = document.querySelector("#myNav");
+myNav.navLinks = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "About",
+    href: "/about",
+  },
+  {
+    name: "Templates",
+    href: "/templates",
+  },
+  {
+    name: "Plugins",
+    href: "/plugins",
+  },
+  {
+    name: "Error",
+    href: "/error",
+  },
+];
