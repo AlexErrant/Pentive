aa9f5c15851fd6419a90a6974958e2f09f3a66c3
Alex <git@pentive.com>
2022-09-01 12:11:14 -0500

    fixed line endings

--- make.sh
+++ make.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# If this file gives you weird errors, it may be due to line endings. Try:
+# find . -type f -name '*.sh' | xargs sed -i -e 's/\r$//'
+# find . -type f -name '.env' | xargs sed -i -e 's/\r$//'
+
 # TODO: automate https://www.shellcheck.net/
 set -euo pipefail # https://stackoverflow.com/a/2871034
 # set -x
