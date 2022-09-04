bd6d7467b0946b8054054ddba2d0a241f1728abf
Alex <git@pentive.com>
2022-09-04 13:22:29 -0500

    made GetUser return 1 statically typed user

--- rirhath/Rirhath/Controllers/UserController.cs
+++ rirhath/Rirhath/Controllers/UserController.cs
@@ -2,6 +2,14 @@
 using Microsoft.AspNetCore.Mvc;
 
 namespace Rirhath.Controllers {
+
+  public class Padawan {
+    public string Name { get; }
+    public DateTimeOffset Created { get; }
+    public string[] Notifications { get; }
+    public string Nooks { get; }
+  }
+
   [ApiController]
   [Route("[controller]")]
   public class UserController : ControllerBase {
@@ -12,11 +20,13 @@ namespace Rirhath.Controllers {
       _context = context;
     }
 
+    // highTODO needs auth
     [HttpGet(Name = "GetUser")]
-    public async Task<dynamic> Get() {
-      var query = "SELECT * FROM padawan";
+    public async Task<Padawan> Get(string name) {
+      var query = "SELECT * FROM padawan where name = @name";
       using var connection = _context.CreateConnection();
-      return await connection.QueryAsync(query);
+      return await connection.QuerySingleAsync<Padawan>(query, new { name });
     }
+
   }
 }
