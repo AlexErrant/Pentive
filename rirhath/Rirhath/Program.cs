d45c8011f39778c973af3cac989b71717bc73b53
Alex <git@pentive.com>
2022-09-04 09:42:05 -0500

    formatting

--- rirhath/Rirhath/Program.cs
+++ rirhath/Rirhath/Program.cs
@@ -1,35 +1,30 @@
-namespace Rirhath
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+﻿namespace Rirhath {
+  public class Program {
+    public static void Main(string[] args) {
+      var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+      // Add services to the container.
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+      builder.Services.AddControllers();
+      // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+      builder.Services.AddEndpointsApiExplorer();
+      builder.Services.AddSwaggerGen();
 
-            var app = builder.Build();
+      var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+      // Configure the HTTP request pipeline.
+      if (app.Environment.IsDevelopment()) {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+      }
 
-            app.UseHttpsRedirection();
+      app.UseHttpsRedirection();
 
-            app.UseAuthorization();
+      app.UseAuthorization();
 
+      app.MapControllers();
 
-            app.MapControllers();
-
-            app.Run();
-        }
+      app.Run();
     }
+  }
 }
