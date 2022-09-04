a63ca3e54a9684f276a3dda4ba79716c539007c2
Alex <git@pentive.com>
2022-09-04 13:49:52 -0500

    added GET_retrieves_weather_forecast

---
+++ rirhath/Test/ApiWebApplicationFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Rirhath;
+
+namespace Test {
+  // https://timdeschryver.dev/blog/refactor-functional-tests-to-support-minimal-web-apis
+  public class ApiWebApplicationFactory : WebApplicationFactory<Program> {
+    protected override void ConfigureWebHost(IWebHostBuilder builder) {
+      builder.ConfigureAppConfiguration(config => { });
+      builder.ConfigureTestServices(services => { });
+    }
+  }
+}
---
+++ rirhath/Test/WeatherForecastControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+namespace Test {
+  public class WeatherForecastControllerTests {
+
+    [Fact]
+    public async Task GET_retrieves_weather_forecast() {
+      await using var application = new ApiWebApplicationFactory();
+      using var client = application.CreateClient();
+
+      var response = await client.GetAsync("/weatherforecast");
+
+      Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
+  }
+}
