c56d2e5794531ab09eda0762a860b72aa597697e
Alex <git@pentive.com>
2022-09-25 19:29:06 -0500

    npm run prettier:write

--- lrpc/src/client.ts
+++ lrpc/src/client.ts
@@ -1,16 +1,16 @@
-import { createTRPCClient } from '@trpc/client';
-import fetch from 'node-fetch';
-import type { AppRouter } from './server';
+import { createTRPCClient } from "@trpc/client"
+import fetch from "node-fetch"
+import type { AppRouter } from "./server"
 
-global.fetch = fetch as any;
+global.fetch = fetch as any
 
-const client = createTRPCClient<AppRouter>({ url: 'http://127.0.0.1:4050' });
+const client = createTRPCClient<AppRouter>({ url: "http://127.0.0.1:4050" })
 
-(async () => {
+;(async () => {
   try {
-    const q = await client.query('greet', { name: 'Erik' });
-    console.log(q);
+    const q = await client.query("greet", { name: "Erik" })
+    console.log(q)
   } catch (error) {
-    console.log('error', error);
+    console.log("error", error)
   }
-})();
+})()
--- lrpc/src/payloadFormatVersionClient.ts
+++ lrpc/src/payloadFormatVersionClient.ts
@@ -1,30 +1,28 @@
-import { createTRPCClient } from '@trpc/client';
-import fetch from 'node-fetch';
-import type { AppRouter } from './server';
+import { createTRPCClient } from "@trpc/client"
+import fetch from "node-fetch"
+import type { AppRouter } from "./server"
 
-global.fetch = fetch as any;
+global.fetch = fetch as any
 
 const httpApiClient = createTRPCClient<AppRouter>({
-  url: 'http://127.0.0.1:4050',
-});
+  url: "http://127.0.0.1:4050",
+})
 const restApiClient = createTRPCClient<AppRouter>({
-  url: 'http://127.0.0.1:4050/dev',
-});
+  url: "http://127.0.0.1:4050/dev",
+})
 
-(async () => {
+;(async () => {
   try {
     // A Very simple client to test showcase both APIGW v1(Rest API) and v2(HTTP API) support with serverless-offline
-    const queryForVersion2 = await httpApiClient.query(
-      'greet',
-      {name:"bob's your uncle"},
-    );
-    console.log(queryForVersion2);
-    const queryForVersion1 = await restApiClient.query(
-      'greet',
-      {name:"bob's your uncle"},
-    );
-    console.log(queryForVersion1);
+    const queryForVersion2 = await httpApiClient.query("greet", {
+      name: "bob's your uncle",
+    })
+    console.log(queryForVersion2)
+    const queryForVersion1 = await restApiClient.query("greet", {
+      name: "bob's your uncle",
+    })
+    console.log(queryForVersion1)
   } catch (error) {
-    console.log('error', error);
+    console.log("error", error)
   }
-})();
+})()
