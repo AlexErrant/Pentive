e332deb1a942515f2c779534d6cfa0beb4003562
Alex <git@pentive.com>
2022-10-08 07:46:24 -0500

    refactor

--- app/src/pages/importer/importer.ts
+++ app/src/pages/importer/importer.ts
@@ -1,7 +1,7 @@
 import { Buffer } from "buffer"
 import { BlobReader, BlobWriter, ZipReader } from "@zip.js/zip.js"
 import { throwExp } from "../../domain/utility"
-import initSqlJs from "sql.js"
+import initSqlJs, { Database } from "sql.js"
 
 export async function importAnki(
   event: Event & {
@@ -9,13 +9,28 @@ export async function importAnki(
     target: Element
   }
 ): Promise<void> {
-  const sql = await initSqlJs({
-    // Required to load the wasm binary asynchronously. Of course, you can host it wherever you want
-    locateFile: (file) => `https://sql.js.org/dist/${file}`,
-  })
   const ankiExport =
     (event.target as HTMLInputElement).files?.item(0) ??
     throwExp("Impossible - there should be a file selected")
+  const db = await getDb(ankiExport)
+  const cols = db.prepare("select * from col")
+  while (cols.step()) {
+    const row = cols.getAsObject()
+    console.log(row)
+  }
+  db.close()
+}
+
+async function getDb(ankiExport: File): Promise<Database> {
+  const sql = await getSql()
+  const sqliteBlob = await getSqliteBlob(ankiExport)
+  const db = new sql.Database(
+    Buffer.from(new Uint8Array(await sqliteBlob.arrayBuffer()))
+  )
+  return db
+}
+
+async function getSqliteBlob(ankiExport: File): Promise<Blob> {
   const ankiEntries = await new ZipReader(
     new BlobReader(ankiExport)
   ).getEntries()
@@ -25,13 +40,12 @@ export async function importAnki(
   const sqliteBlob =
     (await sqlite.getData?.(new BlobWriter())) ??
     throwExp("todo - I don't understand why `getData` is nullable")
-  const db = new sql.Database(
-    Buffer.from(new Uint8Array(await sqliteBlob.arrayBuffer()))
-  )
-  const cols = db.prepare("select * from col")
-  while (cols.step()) {
-    const row = cols.getAsObject()
-    console.log(row)
-  }
-  db.close()
+  return sqliteBlob
+}
+
+async function getSql(): Promise<initSqlJs.SqlJsStatic> {
+  return await initSqlJs({
+    // Required to load the wasm binary asynchronously. Of course, you can host it wherever you want
+    locateFile: (file) => `https://sql.js.org/dist/${file}`,
+  })
 }
