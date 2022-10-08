e8fe9809bc54d7ca0ea3c9dad7479a7ed06ad06d
Alex <git@pentive.com>
2022-10-08 09:55:39 -0500

    some comments

--- app/src/pages/importer/importer.ts
+++ app/src/pages/importer/importer.ts
@@ -1,3 +1,7 @@
+// lowTODO optimize https://sql.js.org/#/?id=use-as-web-worker https://gildas-lormeau.github.io/zip.js/api/interfaces/WorkerConfiguration.html#useWebWorkers
+// prepared statements https://blog.logrocket.com/detailed-look-basic-sqljs-features/
+// pagination https://stackoverflow.com/q/14468586
+
 import { Buffer } from "buffer"
 import { BlobReader, BlobWriter, ZipReader } from "@zip.js/zip.js"
 import { throwExp } from "../../domain/utility"
@@ -10,6 +14,7 @@ export async function importAnki(
   }
 ): Promise<void> {
   const ankiExport =
+    // My mental static analysis says to use `currentTarget`, but it seems to randomly be null, hence `target`. I'm confused but whatever.
     (event.target as HTMLInputElement).files?.item(0) ??
     throwExp("Impossible - there should be a file selected")
   const db = await getDb(ankiExport)
@@ -41,7 +46,9 @@ async function getSqliteBuffer(ankiExport: File): Promise<Buffer> {
     throwExp("`collection.anki2` not found!")
   const blob =
     (await sqlite.getData?.(new BlobWriter())) ??
-    throwExp("todo - I don't understand why `getData` is nullable")
+    throwExp(
+      "Impossible since we're using `getEntries` https://github.com/gildas-lormeau/zip.js/issues/371"
+    )
   const arrayBuffer = await blob.arrayBuffer()
   return Buffer.from(new Uint8Array(arrayBuffer))
 }
