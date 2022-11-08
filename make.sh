1bab58c6d238f8f9d0351d8210ab27b778233925
Alex <git@pentive.com>
2022-11-08 16:28:25 -0600

    deleted youch

--- make.sh
+++ make.sh
@@ -8,29 +8,7 @@
 set -euo pipefail # https://stackoverflow.com/a/2871034
 # set -x
 
-source ./youch/.env
 source ./ivy/.env
-YOUCH_URL=http://localhost:${YOUCH_HOST_PORT}
 
-YOUCH_HOST_PORT=${YOUCH_HOST_PORT} IVY_HOST_PORT=${IVY_HOST_PORT} docker compose down
-YOUCH_HOST_PORT=${YOUCH_HOST_PORT} IVY_HOST_PORT=${IVY_HOST_PORT} docker compose up --detach
-
-printf "Connecting to Youch..."
-# https://stackoverflow.com/a/57660677
-# https://stackoverflow.com/a/21189440
-until curl --output /dev/null --silent --head --fail "${YOUCH_URL}"; do
-    printf '.'
-    sleep 1
-done
-printf "\n...connected!\n\n"
-
-echo "Configuring Youch..."
-curl --silent --show-error --output /dev/null --user "${COUCHDB_USER}:${COUCHDB_PASSWORD}" -X PUT "${YOUCH_URL}/_users"
-curl --silent --show-error --output /dev/null --user "${COUCHDB_USER}:${COUCHDB_PASSWORD}" -X PUT "${YOUCH_URL}/_node/_local/_config/couch_peruser/enable" -d '"true"'
-curl --silent --show-error --output /dev/null --user "${COUCHDB_USER}:${COUCHDB_PASSWORD}" -X PUT "${YOUCH_URL}/_node/_local/_config/chttpd_auth/timeout" -d '"86400"'
-curl --silent --show-error --output /dev/null --user "${COUCHDB_USER}:${COUCHDB_PASSWORD}" -X PUT "${YOUCH_URL}/_node/_local/_config/httpd/enable_cors" -d '"true"'
-curl --silent --show-error --output /dev/null --user "${COUCHDB_USER}:${COUCHDB_PASSWORD}" -X PUT "${YOUCH_URL}/_node/_local/_config/cors/credentials" -d '"true"'
-curl --silent --show-error --output /dev/null --user "${COUCHDB_USER}:${COUCHDB_PASSWORD}" -X PUT "${YOUCH_URL}/_node/_local/_config/cors/origins" -d '"*"' # highTODO make more restrictive
-curl --silent --show-error --output /dev/null --user "${COUCHDB_USER}:${COUCHDB_PASSWORD}" -X PUT "${YOUCH_URL}/_node/_local/_config/cors/methods" -d '"GET, PUT, POST, HEAD, DELETE"'
-curl --silent --show-error --output /dev/null --user "${COUCHDB_USER}:${COUCHDB_PASSWORD}" -X PUT "${YOUCH_URL}/_node/_local/_config/cors/headers" -d '"accept, authorization, content-type, origin, referer"'
-echo "...configured!"
+IVY_HOST_PORT=${IVY_HOST_PORT} docker compose down
+IVY_HOST_PORT=${IVY_HOST_PORT} docker compose up --detach
