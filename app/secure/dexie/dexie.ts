c5c8881a768e5c7183334a2bec0f6608b7986c5e
Alex <git@pentive.com>
2022-11-09 11:32:54 -0600

    log getResource because it'll be called many times if there's a memory leak

--- app/secure/dexie/dexie.ts
+++ app/secure/dexie/dexie.ts
@@ -24,6 +24,8 @@ export const dexieMethods = {
     await ddb.resources.bulkAdd(resources)
   },
   async getResource(id: ResourceId) {
+    // This helps detect memory leaks - if you see this log 100x, something's very wrong.
+    console.debug("getResource for " + id)
     // highTODO perf is abysmal https://stackoverflow.com/q/20809832
     // Consider using Origin Private File System once it's on Android Chrome https://chromestatus.com/feature/5079634203377664 https://bugs.chromium.org/p/chromium/issues/detail?id=1011535#c9
     const resource = await ddb.resources.get(id)
