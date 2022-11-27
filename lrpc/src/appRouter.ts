6cdf95c1d7d6518eb4fba3adeb3293ef2e729b11
Alex <git@pentive.com>
2022-11-27 10:33:40 -0600

    extracted templateRouter

--- lrpc/src/appRouter.ts
+++ lrpc/src/appRouter.ts
@@ -1,83 +1,12 @@
 import { z } from "zod"
-import { createRemoteTemplate, remoteTemplate } from "./schemas/template"
-import { id } from "./schemas/core"
-import _ from "lodash"
-import { Ulid } from "id128"
-import { Prisma, Template } from "@prisma/client"
-import { authedProcedure, publicProcedure, router } from "./trpc"
-import { prisma, ulidStringToBuffer, ulidToBuffer } from "./prisma"
-
-export type ClientTemplate = Omit<Template, "id"> & { id: string }
-
-function mapTemplate(t: Template): ClientTemplate {
-  const id = Ulid.fromRaw(t.id.toString("hex")).toCanonical()
-  return { ...t, id }
-}
+import { templateRouter } from "./templateRouter"
+import { publicProcedure, router } from "./trpc"
 
 export const appRouter = router({
   greeting: publicProcedure
     .input(z.string())
     .query(({ input }) => `hello ${input}!`),
-  addTemplate: authedProcedure
-    .input(
-      z.object({
-        name: z.string(),
-      })
-    )
-    .mutation(async (req) => {
-      const id = Ulid.generate()
-      await prisma.template.create({
-        data: {
-          ...req.input,
-          id: ulidToBuffer(id),
-          nook: "nook",
-          authorId: req.ctx.user,
-          type: "type",
-          fields: "fields",
-          css: "css",
-          childTemplates: "childTemplates",
-          ankiId: 0,
-        },
-      })
-      return id.toCanonical()
-    }),
-  addTemplates: authedProcedure
-    .input(z.array(createRemoteTemplate))
-    .mutation(async (req) => {
-      const templateCreatesAndIds = req.input.map(({ templateType, ...t }) => {
-        const remoteId = Ulid.generate()
-        const t2: Prisma.TemplateCreateManyInput = {
-          ...t,
-          type: templateType,
-          fields: JSON.stringify(t.fields),
-          id: ulidToBuffer(remoteId),
-          authorId: req.ctx.user,
-        }
-        return [t2, [t.id, remoteId.toCanonical()] as [string, string]] as const
-      })
-      const templateCreates = templateCreatesAndIds.map((x) => x[0])
-      const remoteIdByLocal = _.fromPairs(
-        templateCreatesAndIds.map((x) => x[1])
-      )
-      await prisma.template.createMany({
-        data: templateCreates,
-      })
-      return remoteIdByLocal
-    }),
-  getTemplate: publicProcedure.input(id).query(async (req) => {
-    const id = ulidStringToBuffer(req.input)
-    const template = await prisma.template.findUnique({ where: { id } })
-    if (template != null) {
-      return mapTemplate(template)
-    }
-    return template
-  }),
-  getTemplates: publicProcedure.input(z.array(id)).query(async (req) => {
-    const r = await prisma.template.findMany({
-      where: { id: { in: req.input.map(ulidStringToBuffer) } },
-    })
-    return r.map(mapTemplate)
-  }),
+  ...templateRouter,
 })
 
 export type AppRouter = typeof appRouter
---
+++ lrpc/src/templateRouter.ts
@@ -0,0 +1,78 @@
+import { createRemoteTemplate, remoteTemplate } from "./schemas/template"
+import { id } from "./schemas/core"
+import { z } from "zod"
+import _ from "lodash"
+import { Ulid } from "id128"
+import { Prisma, Template } from "@prisma/client"
+import { authedProcedure, publicProcedure } from "./trpc"
+import { prisma, ulidStringToBuffer, ulidToBuffer } from "./prisma"
+
+type ClientTemplate = Omit<Template, "id"> & { id: string }
+
+function mapTemplate(t: Template): ClientTemplate {
+  const id = Ulid.fromRaw(t.id.toString("hex")).toCanonical()
+  return { ...t, id }
+}
+
+export const templateRouter = {
+  addTemplate: authedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+      })
+    )
+    .mutation(async (req) => {
+      const id = Ulid.generate()
+      await prisma.template.create({
+        data: {
+          ...req.input,
+          id: ulidToBuffer(id),
+          nook: "nook",
+          authorId: req.ctx.user,
+          type: "type",
+          fields: "fields",
+          css: "css",
+          childTemplates: "childTemplates",
+          ankiId: 0,
+        },
+      })
+      return id.toCanonical()
+    }),
+  addTemplates: authedProcedure
+    .input(z.array(createRemoteTemplate))
+    .mutation(async (req) => {
+      const templateCreatesAndIds = req.input.map(({ templateType, ...t }) => {
+        const remoteId = Ulid.generate()
+        const t2: Prisma.TemplateCreateManyInput = {
+          ...t,
+          type: templateType,
+          fields: JSON.stringify(t.fields),
+          id: ulidToBuffer(remoteId),
+          authorId: req.ctx.user,
+        }
+        return [t2, [t.id, remoteId.toCanonical()] as [string, string]] as const
+      })
+      const templateCreates = templateCreatesAndIds.map((x) => x[0])
+      const remoteIdByLocal = _.fromPairs(
+        templateCreatesAndIds.map((x) => x[1])
+      )
+      await prisma.template.createMany({
+        data: templateCreates,
+      })
+      return remoteIdByLocal
+    }),
+  getTemplate: publicProcedure.input(id).query(async (req) => {
+    const id = ulidStringToBuffer(req.input)
+    const template = await prisma.template.findUnique({ where: { id } })
+    if (template != null) {
+      return mapTemplate(template)
+    }
+    return template
+  }),
+  getTemplates: publicProcedure.input(z.array(id)).query(async (req) => {
+    const r = await prisma.template.findMany({
+      where: { id: { in: req.input.map(ulidStringToBuffer) } },
+    })
+    return r.map(mapTemplate)
+  }),
+}
