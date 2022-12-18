7e06f2dbb02cae2af6a03d79b31686af1f3bde27
Alex <git@pentive.com>
2022-12-18 12:27:14 -0600

    remove d1

--- mediaRouter/src/index.ts
+++ mediaRouter/src/index.ts
@@ -21,7 +21,6 @@ export interface Env {
   //
   // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
   mediaBucket: R2Bucket
-  mediaDb: D1Database
   jwsPublicKey: string
   jwsPrivateKey: string
 }
