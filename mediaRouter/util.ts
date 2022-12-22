28339a828734b9f210470d310bb394a6061313ad
Alex <git@pentive.com>
2022-12-22 16:23:58 -0600

    added base64ToArrayBuffer and arrayBufferToBase64

---
+++ mediaRouter/util.ts
@@ -0,0 +1,20 @@
+// https://stackoverflow.com/a/38858127/
+export function arrayBufferToBase64(buffer: ArrayBuffer): string {
+  let binary = ""
+  const bytes = new Uint8Array(buffer)
+  const len = bytes.byteLength
+  for (let i = 0; i < len; i++) {
+    binary += String.fromCharCode(bytes[i])
+  }
+  return btoa(binary)
+}
+
+export function base64ToArrayBuffer(base64: string): ArrayBuffer {
+  const binaryString = atob(base64)
+  const len = binaryString.length
+  const bytes = new Uint8Array(len)
+  for (let i = 0; i < len; i++) {
+    bytes[i] = binaryString.charCodeAt(i)
+  }
+  return bytes.buffer
+}
