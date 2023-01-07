50abf805cb7b80fedb2a9e0079a7e8f3cc6cb938
Alex <git@pentive.com>
2023-01-06 21:16:51 -0600

    added submit

---
+++ hub/src/routes/n/[nook]/submit.tsx
@@ -0,0 +1,100 @@
+import { Show, JSX } from "solid-js"
+import { FormError, RouteDataArgs, useRouteData } from "solid-start"
+import { Kysely } from "shared"
+import { Ulid } from "id128"
+import { createServerAction$, redirect } from "solid-start/server"
+import { Hex } from "shared/src/brand"
+import { getUser } from "~/db/session"
+
+export function routeData({ params }: RouteDataArgs) {
+  const nook = (): string => params.nook
+  return { nook }
+}
+
+function validateTitle(title: unknown): string | undefined {
+  if (typeof title !== "string" || title.length < 3) {
+    return `Title must be at least 3 characters long.`
+  }
+}
+
+function validateText(text: unknown): string | undefined {
+  if (typeof text !== "string" || text.length < 6) {
+    return `Text must be at least 6 characters long.`
+  }
+}
+
+function validateNook(nook: unknown): string | undefined {
+  if (typeof nook !== "string" || nook.length < 1) {
+    return `Nook must be at least 1 character long.`
+  }
+}
+
+export default function Submit(): JSX.Element {
+  const { nook } = useRouteData<typeof routeData>()
+
+  const [submitting, { Form }] = createServerAction$(
+    async (form: FormData, { env, request }) => {
+      const title = form.get("title")
+      const text = form.get("text")
+      const nook = form.get("nook")
+      if (
+        typeof title !== "string" ||
+        typeof text !== "string" ||
+        typeof nook !== "string"
+      ) {
+        throw new FormError(`Title, text, and nook should be strings.`)
+      }
+      const fields = { title, text, nook }
+      const fieldErrors = {
+        title: validateTitle(title),
+        text: validateText(text),
+        nook: validateNook(nook),
+      }
+      if (Object.values(fieldErrors).some(Boolean)) {
+        throw new FormError("Some fields are invalid", { fieldErrors, fields })
+      }
+      const user = await getUser(request)
+      if (user == null) throw redirect("/login") as unknown
+
+      await new Kysely(env.planetscaleDbUrl).insertPost({
+        id: Ulid.generate().toRaw() as Hex,
+        authorId: user.username,
+        title,
+        text,
+        nook,
+      })
+      // highTODO return redirect to post
+    }
+  )
+  const error = (): FormError | undefined =>
+    submitting.error as undefined | FormError
+
+  // highTODO add CSRF & idempotency token
+
+  return (
+    <main>
+      <h1>Submit new Post</h1>
+      <Form>
+        <input type="hidden" name="nook" value={nook()} />
+        <div>
+          <label for="title-input">Title</label>
+          <input id="title-input" name="title" />
+        </div>
+        <Show when={error()?.fieldErrors?.title}>
+          <p>{error()!.fieldErrors!.title}</p>
+        </Show>
+        <div>
+          <label for="text-input">Text</label>
+          <textarea id="text-input" name="text" rows="4" cols="50"></textarea>
+        </div>
+        <Show when={error()?.fieldErrors?.text}>
+          <p>{error()!.fieldErrors!.text}</p>
+        </Show>
+        <Show when={error()}>
+          <p>{error()!.message}</p>
+        </Show>
+        <button type="submit">Submit</button>
+      </Form>
+    </main>
+  )
+}
--- shared/src/kysely.ts
+++ shared/src/kysely.ts
@@ -1,7 +1,7 @@
-import { Kysely as RealKysely, sql } from "kysely"
+import { Kysely as RealKysely, sql, InsertResult, RawBuilder } from "kysely"
 import { PlanetScaleDialect } from "kysely-planetscale"
 import { DB, Post } from "./database"
-import { Base64 } from "./brand"
+import { Base64, Hex } from "./brand"
 
 export class Kysely {
   #db: RealKysely<DB>
@@ -30,4 +30,33 @@ export class Kysely {
       .where("nook", "=", nook)
       .execute()
   }
+
+  async insertPost({
+    authorId,
+    nook,
+    text,
+    title,
+    id,
+  }: {
+    authorId: string
+    nook: string
+    text: string
+    title: string
+    id: Hex
+  }): Promise<InsertResult[]> {
+    return await this.#db
+      .insertInto("Post")
+      .values({
+        id: unhex(id),
+        authorId,
+        nook,
+        text,
+        title,
+      })
+      .execute()
+  }
+}
+
+function unhex(id: Hex): RawBuilder<Base64> {
+  return sql<Base64>`UNHEX(${id})` // nextTODO fix
 }
