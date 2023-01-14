838cc96086044159d3b91d539c3e25647e0702cc
Alex <git@pentive.com>
2023-01-13 19:38:46 -0600

    added ssl cert

--- hub/vite.config.ts
+++ hub/vite.config.ts
@@ -1,6 +1,7 @@
 import solid from "solid-start/vite"
 import { defineConfig } from "vite"
 import cloudflare from "solid-start-cloudflare-workers"
+import fs from "fs"
 
 export default defineConfig({
   plugins: [
@@ -8,4 +9,16 @@ export default defineConfig({
       adapter: cloudflare({ envPath: true }),
     }),
   ],
+  server: {
+    https: {
+      key: fs.readFileSync("./.cert/key.pem"),
+      cert: fs.readFileSync("./.cert/cert.pem"),
+    },
+  },
+  preview: {
+    https: {
+      key: fs.readFileSync("./.cert/key.pem"),
+      cert: fs.readFileSync("./.cert/cert.pem"),
+    },
+  },
 })
