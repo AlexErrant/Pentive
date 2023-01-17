b1ce6ce3dc8adc87c170a41096d30075b6cdd582
Alex <git@pentive.com>
2023-01-17 14:35:27 -0600

    https://stackoverflow.com/a/54745526

--- app/secure/sqlite/crsqlite.ts
+++ app/secure/sqlite/crsqlite.ts
@@ -16,7 +16,7 @@ async function createDb(): Promise<sqliteWasm.DB> {
   const sqlite = await sqliteWasm.default(
     (file) =>
       // need to update this version number 👇👇 after every upgrade. lowTODO figure out something better. Grep for F00E815A-C0FD-4AEA-B83C-0BDB641D97CC
-      "https://esm.sh/@vlcn.io/wa-crsqlite@0.4.2/dist/wa-sqlite-async.wasm"
+      "https://esm.sh/@vlcn.io/wa-crsqlite@0.7.1/dist/wa-sqlite-async.wasm"
   )
   const db = await sqlite.open("username.db")
   await db.execMany(initSql)
