4d765e0d6705d576491b6ad1f1d2f201ac4f16d4
Alex <git@pentive.com>
2023-01-24 17:39:02 -0600

    added buildHeaders.ts

---
+++ app-ugc/buildHeaders.ts
@@ -0,0 +1,11 @@
+import { hstsName, hstsValue } from "shared"
+import * as fs from "fs"
+
+const contents = `/*
+  ${hstsName}: ${hstsValue}`
+
+fs.writeFile("./dist/_headers", contents, (err) => {
+  if (err != null) {
+    console.error(err)
+  }
+})
