f6035cd27a39a80948ccb3c3b79fab471b5a3b2b
Alex <git@pentive.com>
2023-02-07 11:02:50 -0600

    fix database schema

--- app/src/sqlite/database.ts
+++ app/src/sqlite/database.ts
@@ -1,29 +1,31 @@
+import { DbId } from "shared"
+
 export interface Note {
-  id: Buffer | null;
-  templateId: Buffer | null;
-  pushId: Buffer | null;
-  pushTemplateId: Buffer | null;
-  push: string | null;
-  ankiNoteId: string | null;
-  created: string | null;
-  modified: string | null;
-  tags: string | null;
-  fieldValues: string | null;
+  id: DbId
+  templateId: DbId | null
+  pushId: DbId | null
+  pushTemplateId: DbId | null
+  push: number | null
+  ankiNoteId: number | null
+  created: number | null
+  modified: number | null
+  tags: string | null
+  fieldValues: string | null
 }
 
 export interface Template {
-  id: Buffer | null;
-  pushId: Buffer | null;
-  push: string | null;
-  name: string | null;
-  css: string | null;
-  fields: string | null;
-  created: string | null;
-  modified: string | null;
-  templateType: string | null;
+  id: DbId
+  pushId: DbId | null
+  push: number | null
+  name: string | null
+  css: string | null
+  fields: string | null
+  created: number | null
+  modified: number | null
+  templateType: string | null
 }
 
 export interface DB {
-  note: Note;
-  template: Template;
+  note: Note
+  template: Template
 }
