01326406eeb14489e14f15ca945bc27107461129
Alex <git@pentive.com>
2023-02-10 18:53:43 -0600

    update hono

--- mediaRouter/src/index.ts
+++ mediaRouter/src/index.ts
@@ -38,7 +38,8 @@ import {
 } from "../privateToken"
 
 type MediaRouterContext = Context<
-  never,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  any,
   {
     // eslint-disable-next-line @typescript-eslint/naming-convention
     Bindings: Env
