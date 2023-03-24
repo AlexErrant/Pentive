95634b53ba43d0cc0aebd8d1a28e5787c4fddbdb
Alex <git@pentive.com>
2023-03-24 18:33:02 -0500

    8787 => 3017

--- cwa/src/index.ts
+++ cwa/src/index.ts
@@ -2,7 +2,7 @@
  * Welcome to Cloudflare Workers! This is your first worker.
  *
  * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Open a browser tab at http://localhost:3017/ to see your worker in action
  * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
  *
  * Learn more at https://developers.cloudflare.com/workers/
