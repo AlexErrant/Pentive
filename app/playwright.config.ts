43d9cf2775ea42589d06fa9a3840fb3b6db9ba85
Alex <git@pentive.com>
2023-03-24 19:09:22 -0500

    playwright uses 3012

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -36,7 +36,7 @@ const config: PlaywrightTestConfig = {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:3013",
+    baseURL: "http://localhost:3012",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -99,8 +99,8 @@ const config: PlaywrightTestConfig = {
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run dev -- --port 3013",
-    port: 3013,
+    command: "npm run dev -- --port 3012",
+    port: 3012,
   },
 }
 
