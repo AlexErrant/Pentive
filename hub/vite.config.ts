5f74a646089fb89e31affaa301b703705f440be8
Alex <git@pentive.com>
2023-04-06 18:36:11 -0500

    set miniflare upstream
    https://github.com/solidjs/solid-start/issues/834

--- hub/vite.config.ts
+++ hub/vite.config.ts
@@ -1,28 +1,39 @@
 import solid from "solid-start/vite"
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import cloudflare from "solid-start-cloudflare-workers"
 import fs from "fs"
 
-export default defineConfig({
-  plugins: [
-    solid({
-      adapter: cloudflare({ envPath: true }),
-    }),
-  ],
-  server: {
-    port: 3014,
-    strictPort: true,
-    https: {
-      key: fs.readFileSync("./.cert/key.pem"),
-      cert: fs.readFileSync("./.cert/cert.pem"),
+const port = 3014
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd()) as ImportMetaEnv
+  return {
+    plugins: [
+      solid({
+        adapter: cloudflare({
+          envPath: true,
+          upstream:
+            mode === "development"
+              ? `https://${env.VITE_HUB_DOMAIN}:${port}`
+              : undefined,
+        }),
+      }),
+    ],
+    server: {
+      port,
+      strictPort: true,
+      https: {
+        key: fs.readFileSync("./.cert/key.pem"),
+        cert: fs.readFileSync("./.cert/cert.pem"),
+      },
     },
-  },
-  preview: {
-    port: 3014,
-    strictPort: true,
-    https: {
-      key: fs.readFileSync("./.cert/key.pem"),
-      cert: fs.readFileSync("./.cert/cert.pem"),
+    preview: {
+      port,
+      strictPort: true,
+      https: {
+        key: fs.readFileSync("./.cert/key.pem"),
+        cert: fs.readFileSync("./.cert/cert.pem"),
+      },
     },
-  },
+  }
 })
