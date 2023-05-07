4d85a420c0a4e7ecceffdfbdcb71e70d1593231a
Alex <git@pentive.com>
2023-05-06 20:09:14 -0500

    npx ts-node

--- hub-ugc/deploy.sh
+++ hub-ugc/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 npm run build
-ts-node --esm --project ./tsconfig.deploy.json buildHeaders.ts
+npx ts-node --esm --project ./tsconfig.deploy.json buildHeaders.ts
 source env.sh
 npx wrangler pages publish ./dist --project-name hub-ugc --branch main
