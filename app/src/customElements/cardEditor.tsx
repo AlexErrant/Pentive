afe817534ad5b655e65bfee54d5251afd0a7209c
Alex <git@pentive.com>
2023-05-18 15:14:47 -0500

    extracted CardRemote

--- app/src/customElements/cardEditor.tsx
+++ app/src/customElements/cardEditor.tsx
@@ -1,91 +1,15 @@
-import {
-  createResource,
-  For,
-  type Setter,
-  Show,
-  type VoidComponent,
-} from "solid-js"
+import { For, type VoidComponent } from "solid-js"
 import ResizingIframe from "../customElements/resizingIframe"
-import {
-  type NoteCard,
-  type NookId,
-  type NoteId,
-  type RemoteNoteId,
-  type RemoteTemplateId,
-} from "shared"
-import { db } from "../db"
+import { type NoteCard } from "shared"
 import { FieldEditor } from "./fieldEditor"
-
-function toggleNook(
-  uploadable: boolean,
-  noteId: NoteId,
-  nook: NookId,
-  setRemotes: Setter<
-    Array<{
-      readonly nookId: NookId
-      readonly remoteTemplateId: RemoteTemplateId | null
-      readonly remoteNoteId: RemoteNoteId | null
-      readonly uploadable: boolean
-    }>
-  >
-) {
-  return (
-    <input
-      type="checkbox"
-      checked={uploadable}
-      onChange={async () => {
-        uploadable = !uploadable
-        setRemotes((rs) =>
-          rs.map((r) => (r.nookId === nook ? { ...r, uploadable } : r))
-        )
-        uploadable
-          ? await db.makeNoteUploadable(noteId, nook)
-          : await db.makeNoteNotUploadable(noteId, nook)
-      }}
-    />
-  )
-}
+import { CardRemote } from "./cardRemote"
 
 export const CardEditor: VoidComponent<{
   readonly noteCard: NoteCard
 }> = (props) => {
-  const [getRemotes, { mutate: setRemotes }] = createResource(
-    () => props.noteCard,
-    ({ note, template }) =>
-      Array.from(template.remotes).map(([nookId, remoteTemplateId]) => {
-        const remoteNoteId = note.remotes.get(nookId) ?? null
-        const uploadable = note.remotes.has(nookId)
-        return {
-          nookId,
-          remoteTemplateId,
-          remoteNoteId,
-          uploadable,
-        } as const
-      }),
-    { initialValue: [] }
-  )
   return (
     <>
-      <For each={getRemotes()}>
-        {(x) => (
-          <li class="py-2 px-4">
-            {x.uploadable ? "✔" : ""}
-            <Show when={x.remoteNoteId != null} fallback={x.nookId}>
-              <a
-                href={`${import.meta.env.VITE_HUB_ORIGIN}/n/${x.remoteNoteId!}`}
-              >
-                {x.nookId}
-              </a>
-            </Show>
-            {toggleNook(
-              x.uploadable,
-              props.noteCard.note.id,
-              x.nookId,
-              setRemotes
-            )}
-          </li>
-        )}
-      </For>
+      <CardRemote noteCard={props.noteCard} />
       <For each={Array.from(props.noteCard.note.fieldValues.entries() ?? [])}>
         {([field, value]) => FieldEditor({ field, value })}
       </For>
---
+++ app/src/customElements/cardRemote.tsx
@@ -0,0 +1,89 @@
+import {
+  createResource,
+  For,
+  type Setter,
+  Show,
+  type VoidComponent,
+} from "solid-js"
+import {
+  type NoteCard,
+  type NookId,
+  type NoteId,
+  type RemoteNoteId,
+  type RemoteTemplateId,
+} from "shared"
+import { db } from "../db"
+
+function toggleNook(
+  uploadable: boolean,
+  noteId: NoteId,
+  nook: NookId,
+  setRemotes: Setter<
+    Array<{
+      readonly nookId: NookId
+      readonly remoteTemplateId: RemoteTemplateId | null
+      readonly remoteNoteId: RemoteNoteId | null
+      readonly uploadable: boolean
+    }>
+  >
+) {
+  return (
+    <input
+      type="checkbox"
+      checked={uploadable}
+      onChange={async () => {
+        uploadable = !uploadable
+        setRemotes((rs) =>
+          rs.map((r) => (r.nookId === nook ? { ...r, uploadable } : r))
+        )
+        uploadable
+          ? await db.makeNoteUploadable(noteId, nook)
+          : await db.makeNoteNotUploadable(noteId, nook)
+      }}
+    />
+  )
+}
+
+export const CardRemote: VoidComponent<{
+  readonly noteCard: NoteCard
+}> = (props) => {
+  const [getRemotes, { mutate: setRemotes }] = createResource(
+    () => props.noteCard,
+    ({ note, template }) =>
+      Array.from(template.remotes).map(([nookId, remoteTemplateId]) => {
+        const remoteNoteId = note.remotes.get(nookId) ?? null
+        const uploadable = note.remotes.has(nookId)
+        return {
+          nookId,
+          remoteTemplateId,
+          remoteNoteId,
+          uploadable,
+        } as const
+      }),
+    { initialValue: [] }
+  )
+  return (
+    <>
+      <For each={getRemotes()}>
+        {(x) => (
+          <li class="py-2 px-4">
+            {x.uploadable ? "✔" : ""}
+            <Show when={x.remoteNoteId != null} fallback={x.nookId}>
+              <a
+                href={`${import.meta.env.VITE_HUB_ORIGIN}/n/${x.remoteNoteId!}`}
+              >
+                {x.nookId}
+              </a>
+            </Show>
+            {toggleNook(
+              x.uploadable,
+              props.noteCard.note.id,
+              x.nookId,
+              setRemotes
+            )}
+          </li>
+        )}
+      </For>
+    </>
+  )
+}
