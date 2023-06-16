541939b0f457f313e0a97b3159056f14fb16ab79
Alex <git@pentive.com>
2023-06-16 15:08:22 -0500

    /cards uses a store

--- app/src/pages/cards.tsx
+++ app/src/pages/cards.tsx
@@ -1,4 +1,5 @@
-import { createSignal, type JSX, Show } from "solid-js"
+import { type JSX, Show } from "solid-js"
+import { createStore } from "solid-js/store"
 import CardsTable from "../components/cardsTable"
 import { type NoteCard } from "shared"
 import { CardsRemote } from "../components/cardsRemote"
@@ -6,22 +7,22 @@ import { FieldsEditor } from "../components/fieldsEditor"
 import { CardsPreview } from "../components/cardsPreview"
 
 export default function Cards(): JSX.Element {
-  const [selected, setSelected] = createSignal<NoteCard>()
+  const [selected, setSelected] = createStore<{ selected?: NoteCard }>({})
   return (
     <>
       <CardsTable
         onSelectionChanged={(ncs) => {
           if (ncs.length > 0) {
-            setSelected(ncs[0])
+            setSelected("selected", ncs[0])
           } else {
-            setSelected(undefined)
+            setSelected("selected", undefined)
           }
         }}
       />
-      <Show when={selected() != null}>
-        <CardsRemote noteCard={selected()!} />
-        <FieldsEditor noteCard={selected()!} />
-        <CardsPreview noteCard={selected()!} />
+      <Show when={selected.selected != null}>
+        <CardsRemote noteCard={selected.selected!} />
+        <FieldsEditor noteCard={selected.selected!} />
+        <CardsPreview noteCard={selected.selected!} />
       </Show>
     </>
   )
