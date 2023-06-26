050b56d7ef0e84b6efe7a2fe3407e88d274cb98f
Alex <git@pentive.com>
2023-06-26 13:58:14 -0500

    convert image dataurl to db entry on persist

--- app/src/components/fieldsEditor.tsx
+++ app/src/components/fieldsEditor.tsx
@@ -4,7 +4,10 @@ import { toNoteCards, type NoteCardView } from "../pages/cards"
 import { type SetStoreFunction } from "solid-js/store"
 import { db } from "../db"
 import { getKysely } from "../sqlite/crsqlite"
-import { throwExp } from "shared"
+import { type MediaId, throwExp } from "shared"
+import { type Transaction } from "kysely"
+import { type DB } from "../sqlite/database"
+import { ulidAsBase64Url } from "../domain/utility"
 
 export const FieldsEditor: VoidComponent<{
   readonly noteCard: NoteCardView
@@ -26,10 +29,27 @@ export const FieldsEditor: VoidComponent<{
       </For>
       <button
         onClick={async () => {
-          const noteCards = toNoteCards(props.noteCard)
-          if (noteCards.length === 0) throwExp("There must be at least 1 card")
+          if (props.noteCard.cards.length === 0)
+            throwExp("There must be at least 1 card")
           const kysely = await getKysely()
+          const dp = new DOMParser()
+          // eslint-disable-next-line solid/reactivity -- the fn isn't reactive
           await kysely.transaction().execute(async (trx) => {
+            const fieldValues = await Promise.all(
+              props.noteCard.note.fieldValues.map(async ([f, v]) => {
+                const doc = dp.parseFromString(v, "text/html")
+                await Promise.all(
+                  Array.from(doc.images).map(async (i) => {
+                    await mutate(i, trx)
+                  })
+                )
+                return [f, doc.body.innerHTML] as const
+              })
+            )
+            const noteCards = toNoteCards({
+              ...props.noteCard,
+              note: { ...props.noteCard.note, fieldValues },
+            })
             await db.upsertNote(noteCards[0].note, trx)
             await db.bulkUpsertCards(
               noteCards.map((nc) => nc.card),
@@ -42,4 +62,27 @@ export const FieldsEditor: VoidComponent<{
       </button>
     </>
   )
+}
+
+async function mutate(img: HTMLImageElement, trx: Transaction<DB>) {
+  const src = img.getAttribute("src")
+  if (src == null || src === "") {
+    // do nothing
+  } else if (src.startsWith("data:")) {
+    const now = new Date()
+    const data = await (await fetch(src)).arrayBuffer()
+    const id = ulidAsBase64Url() as string as MediaId
+    await db.insertMediaTrx(
+      {
+        id,
+        created: now,
+        updated: now,
+        data,
+      },
+      trx
+    )
+    img.setAttribute("src", id)
+  } else {
+    // do nothing, is a regular URL
+  }
 }
