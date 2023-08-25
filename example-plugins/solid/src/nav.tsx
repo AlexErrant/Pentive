a9aeea814f8ab09a036bd7922f9f3a1bf5ee7298
Alex <git@pentive.com>
2023-08-24 19:21:10 -0500

    solid plugin can use solid's router

--- example-plugins/solid/src/nav.tsx
+++ example-plugins/solid/src/nav.tsx
@@ -2,21 +2,24 @@
 
 import { For, type VoidComponent } from "solid-js"
 import { type NavLinkData } from "app/src/components/contracts"
+import { NavLink, Router } from "@solidjs/router"
 
 export const Nav: VoidComponent<{ navLinks: NavLinkData[] }> = (props) => {
   return (
-    <nav class="bg-gray-200 text-gray-900 px-4">
-      <ul class="flex items-center">
-        <For each={props.navLinks}>
-          {({ href, child }) => (
-            <li class="py-2 px-4">
-              <a href={href} class="no-underline hover:underline">
-                {typeof child === "function" ? child() : child}
-              </a>
-            </li>
-          )}
-        </For>
-      </ul>
-    </nav>
+    <Router>
+      <nav class="bg-gray-200 text-gray-900 px-4">
+        <ul class="flex items-center">
+          <For each={props.navLinks}>
+            {({ href, child }) => (
+              <li class="py-2 px-4">
+                <NavLink href={href} class="no-underline hover:underline">
+                  {typeof child === "function" ? child() : child}
+                </NavLink>
+              </li>
+            )}
+          </For>
+        </ul>
+      </nav>
+    </Router>
   )
 }
