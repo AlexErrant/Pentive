e2fac9131b516e447ae710471703a01317cb5c4a
Alex <git@pentive.com>
2023-09-23 20:00:39 -0500

    fix warning

--- app/src/app.tsx
+++ app/src/app.tsx
@@ -1,11 +1,18 @@
-import type { JSX } from 'solid-js'
+import { type JSX, createEffect } from 'solid-js'
 import { useRoutes } from '@solidjs/router'
-
 import { navLinks, routes } from './routes'
 import { C } from './pluginManager'
 import { Toaster } from 'solid-toast'
+import 'golden-layout/dist/css/goldenlayout-base.css'
+import { theme } from './globalState'
 
 export default function App(): JSX.Element {
+	createEffect(() => {
+		if (theme() === 'light') {
+			import('golden-layout/dist/css/themes/goldenlayout-light-theme.css')
+		} else import('golden-layout/dist/css/themes/goldenlayout-dark-theme.css')
+	})
+
 	const Route = useRoutes(routes)
 
 	return (
--- app/src/globalState.ts
+++ app/src/globalState.ts
@@ -1,4 +1,4 @@
-import { createEffect, createResource, createSignal, untrack } from 'solid-js'
+import { createResource, createSignal, untrack } from 'solid-js'
 import { cwaClient } from './trpcClient'
 
 // lowTODO have hub send app a message when a successful login occurs
@@ -24,11 +24,3 @@ new MutationObserver((_: MutationRecord[]) => {
 
 export const agGridTheme = () =>
 	theme() === 'light' ? 'ag-theme-alpine' : 'ag-theme-alpine-dark'
-
-// eslint-disable-next-line import/first -- let's just group all the golden-layout css here
-import 'golden-layout/dist/css/goldenlayout-base.css'
-createEffect(() => {
-	if (theme() === 'light') {
-		import('golden-layout/dist/css/themes/goldenlayout-light-theme.css')
-	} else import('golden-layout/dist/css/themes/goldenlayout-dark-theme.css')
-})
