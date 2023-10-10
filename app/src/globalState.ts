d5a2f0f3f2a10536036c6e9475d00addac4e8b45
Alex <git@pentive.com>
2023-10-09 20:05:44 -0500

    return undefined if error or offline

--- app/src/globalState.ts
+++ app/src/globalState.ts
@@ -1,9 +1,18 @@
 import { createResource, createSignal, untrack } from 'solid-js'
 import { cwaClient } from './trpcClient'
+import { toastError, toastInfo } from './components/toasts'
 
 // lowTODO have hub send app a message when a successful login occurs
 export const [whoAmI] = createResource(
-	async () => await cwaClient.whoAmI.query(),
+	async () =>
+		await cwaClient.whoAmI.query().catch((e) => {
+			if (window.navigator.onLine) {
+				toastError('Error occurred while verifying identity.', e)
+			} else {
+				toastInfo('Cannot validate identity while offline.')
+			}
+			return undefined
+		}),
 )
 
 const currentTheme = () =>
