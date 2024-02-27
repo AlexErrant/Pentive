2a8b9efa791f0ea51b185792307b700a13268b87
Alex <git@pentive.com>
2024-02-26 20:32:42 -0600

    ci doesn't run mkenv.sh, so env.sh might not be there

--- hub-ugc/deploy.sh
+++ hub-ugc/deploy.sh
@@ -6,5 +6,5 @@ set -euo pipefail # https://stackoverflow.com/a/2871034
 
 npm run build
 npx ts-node --esm --project ./tsconfig.deploy.json buildHeaders.ts
-source env.sh
+[[ -f "env.sh" ]] && source env.sh
 npx wrangler pages deploy ./dist --project-name hub-ugc --branch main
