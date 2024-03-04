ea6329950c1798784e3b44408ac4e96bc01ef511
Alex <git@pentive.com>
2024-03-03 21:35:47 -0600

    polish

--- shared-dom/src/language/template.highlight.ts
+++ shared-dom/src/language/template.highlight.ts
@@ -3,9 +3,9 @@ import { styleTags, tags as t } from '@lezer/highlight'
 
 export const xmlHighlighting = styleTags({
 	Text: t.content,
-	'StartTag StartCloseTag EndTag SelfCloseEndTag': t.brace,
+	'StartTag StartCloseTag EndTag': t.brace,
 	TagName: t.tagName,
-	'MismatchedCloseTag/Tagname': [t.tagName, t.invalid],
+	'MismatchedCloseTag/TagName': [t.tagName, t.invalid],
 	Transformer: t.attributeName,
 	TransformerDelimiter: t.separator,
 	If: t.controlOperator,
