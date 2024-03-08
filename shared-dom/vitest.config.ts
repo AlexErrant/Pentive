ad7c793c9447a3bd9094c9f827341ec5f82c34e0
Alex <git@pentive.com>
2024-03-08 13:43:27 -0600

    add solidPlugin to vitest

--- shared-dom/vitest.config.ts
+++ shared-dom/vitest.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vitest/config'
+import solidPlugin from 'vite-plugin-solid'
 
 export default defineConfig({
+	plugins: [solidPlugin()],
 	test: {
 		environment: 'jsdom',
 	},
