1a35d2940d3bde2faa9ad3e92e551673806a5710
Alex <git@pentive.com>
2024-03-10 16:07:09 -0500

    can fold Elements

--- shared-dom/src/language/htmlTemplateParser.ts
+++ shared-dom/src/language/htmlTemplateParser.ts
@@ -1,10 +1,16 @@
 import { parser as templateParser } from './templateParser'
 import { htmlLanguage } from '@codemirror/lang-html'
-import { LRLanguage } from '@codemirror/language'
+import { LRLanguage, foldInside, foldNodeProp } from '@codemirror/language'
 import { parseMixed } from '@lezer/common'
 import { Text } from './templateParser.terms'
 
 const htmlTemplateParser = templateParser.configure({
+	props: [
+		foldNodeProp.add({
+			// eslint-disable-next-line @typescript-eslint/naming-convention
+			Element: foldInside,
+		}),
+	],
 	wrap: parseMixed((node) => {
 		return node.type.isTop
 			? {
