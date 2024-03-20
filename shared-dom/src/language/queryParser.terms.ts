6528e08a79e90a9f8dbd3b0327935b875a685124
Alex <git@pentive.com>
2024-03-20 16:10:46 -0500

    add Wildcard to tokens

--- shared-dom/src/language/queryParser.terms.ts
+++ shared-dom/src/language/queryParser.terms.ts
@@ -6,8 +6,9 @@ export const
   Not = 2,
   SimpleString = 3,
   QuotedString = 4,
-  Group = 5,
-  Tag = 6,
-  Deck = 7,
-  Template = 8,
-  Or = 9
+  Wildcard = 5,
+  Group = 6,
+  Tag = 7,
+  Deck = 8,
+  Template = 9,
+  Or = 10
