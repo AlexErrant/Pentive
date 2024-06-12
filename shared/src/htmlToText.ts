7a50e9b95954df6a1b625f9d3749aa87c45c23e0
Alex <git@pentive.com>
2024-06-12 18:55:34 -0500

    add caseFold

--- shared/src/htmlToText.ts
+++ shared/src/htmlToText.ts
@@ -13,7 +13,7 @@ const ftsNormalizeHelper = compile({
 export function ftsNormalize(html: string, removeCombiningCharacters: boolean) {
 	let r = ftsNormalizeHelper(html).replace(/\s+/g, ' ').trim()
 	if (removeCombiningCharacters) {
-		// https://stackoverflow.com/a/37511463
+		// https://stackoverflow.com/a/37511463 also grep 7D669B43-CEA7-45E6-AB82-D2B18C20D633
 		r = r.normalize('NFKD').replace(/[\u0300-\u036f]/g, '')
 	}
 	return r
