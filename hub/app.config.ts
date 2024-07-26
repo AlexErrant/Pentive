fdfbc02fb00e3aa2c826cb9af1aef5c8c34da85b
Alex <git@pentive.com>
2024-07-26 16:05:50 -0500

    https/certs/port work

--- hub/app.config.ts
+++ hub/app.config.ts
@@ -1,34 +1,20 @@
 import { defineConfig } from '@solidjs/start/config'
 import checker from 'vite-plugin-checker'
-// import { loadEnv } from 'vite'
-// import fs from 'fs'
-
-const port = 3014
 
 export default defineConfig({
+	server: {
+		preset: 'cloudflare-module',
+		rollupConfig: {
+			external: ['node:async_hooks'],
+		},
+		https: {
+			key: './.cert/key.pem',
+			cert: './.cert/cert.pem',
+		},
+	},
 	vite: () => {
-		// const env = loadEnv(mode, process.cwd()) as ImportMetaEnv
-		// const keyPath = './.cert/key.pem'
-		// const certPath = './.cert/cert.pem'
-		let key
-		let cert
-		// nextTODO
-		// if (mode === 'development') {
-		// 	key = fs.readFileSync(keyPath)
-		// 	cert = fs.readFileSync(certPath)
-		// }
 		return {
 			plugins: [
-				// nextTODO
-				// solid({
-				// 	adapter: cloudflare({
-				// 		envPath: true,
-				// 		upstream:
-				// 			mode === 'development'
-				// 				? `https://${env.VITE_HUB_DOMAIN}:${port}`
-				// 				: undefined,
-				// 	}),
-				// }),
 				checker({
 					overlay: {
 						initialIsOpen: false,
@@ -41,14 +27,6 @@ export default defineConfig({
 			],
 			build: {
 				sourcemap: true,
-			},
-			server: {
-				port,
-				strictPort: true,
-				https: {
-					key,
-					cert,
-				},
 			},
 		}
 	},
