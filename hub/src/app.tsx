b79744e55cfb6db36c194156ed3834bb88eaf4bb
Alex <git@pentive.com>
2024-08-11 15:56:01 -0500

    drop ErrorBoundary

--- hub/src/app.tsx
+++ hub/src/app.tsx
@@ -1,6 +1,6 @@
 import { Router } from '@solidjs/router'
 import { FileRoutes } from '@solidjs/start/router'
-import { ErrorBoundary, Suspense } from 'solid-js'
+import { Suspense } from 'solid-js'
 import './app.css'
 import Nav from './components/nav'
 
@@ -8,14 +8,10 @@ export default function App() {
 	return (
 		<Router
 			root={(props) => (
-				<ErrorBoundary
-					fallback={(err) => <div>Error: {JSON.stringify(err)}</div>}
-				>
-					<Suspense fallback={<div class='news-list-nav'>Loading...</div>}>
-						<Nav />
-						{props.children}
-					</Suspense>
-				</ErrorBoundary>
+				<Suspense fallback={<div class='news-list-nav'>Loading...</div>}>
+					<Nav />
+					{props.children}
+				</Suspense>
 			)}
 		>
 			<FileRoutes />
