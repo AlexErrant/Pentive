36d30a4c4654f84c25af3330015398c0fcb1514d
Alex <git@pentive.com>
2024-08-19 11:47:15 -0500

    load service worker after rendering

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,6 +1,10 @@
-import './registerServiceWorker'
 import './index.css'
 import { render } from 'solid-js/web'
 import App from './app'
+import { C } from './topLevelAwait'
 
 render(() => <App />, document.getElementById('root') as HTMLElement)
+
+import('./registerServiceWorker').catch((e) => {
+	C.toastError('Error registering service worker.', e)
+})
