39732a31608ffc738d3743b43db09b0f935f6f54
Alex <git@pentive.com>
2024-08-25 09:05:26 -0500

    fix `generateSql`

--- shared/sql/generateSql.ts
+++ shared/sql/generateSql.ts
@@ -1,18 +1,23 @@
 import * as fs from 'fs'
+import * as chokidar from 'chokidar'
 
-const note = fs.readFileSync('./sql/note.sql').toString()
-const card = fs.readFileSync('./sql/card.sql').toString()
-const init = fs.readFileSync('./sql/init.sql').toString()
+chokidar
+	.watch(['./sql/init.sql', './sql/note.sql', './sql/card.sql'])
+	.on('change', (event, path) => {
+		const note = fs.readFileSync('./sql/note.sql').toString()
+		const card = fs.readFileSync('./sql/card.sql').toString()
+		const init = fs.readFileSync('./sql/init.sql').toString()
 
-const sql = [note, card, init]
-	.join('\n')
-	.split(';')
-	.map((x) => x.trim())
-	.filter((x) => x !== '')
-	.map((x) => x + ';')
+		const sql = [note, card, init]
+			.join('\n')
+			.split(';')
+			.map((x) => x.trim())
+			.filter((x) => x !== '')
+			.map((x) => x + ';')
 
-fs.writeFile('./src/sql.json', JSON.stringify(sql, null, 4), (err) => {
-	if (err != null) {
-		console.error(err)
-	}
-})
+		fs.writeFile('./src/sql.json', JSON.stringify(sql, null, 4), (err) => {
+			if (err != null) {
+				console.error(err)
+			}
+		})
+	})
