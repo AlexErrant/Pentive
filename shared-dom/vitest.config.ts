f03a34f6ddb1a75e7aa1b23aec99ba41ec2c640f
Alex <git@pentive.com>
2024-09-05 17:03:36 -0500

    exclude lib

--- shared-dom/vitest.config.ts
+++ shared-dom/vitest.config.ts
@@ -5,6 +5,14 @@ export default defineConfig({
 	plugins: [solidPlugin()],
 	test: {
 		environment: 'jsdom',
+		exclude: [
+			'**/node_modules/**',
+			'**/dist/**',
+			'**/lib/**',
+			'**/cypress/**',
+			'**/.{idea,git,cache,output,temp}/**',
+			'**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*',
+		],
 	},
 	resolve: {
 		conditions: ['development', 'browser'],
