9adb49b7f190d695b1c16ed3332b48e8debfafc7
Alex <git@pentive.com>
2024-09-06 18:13:19 -0500

    don't build

--- app-ugc/deploy.sh
+++ app-ugc/deploy.sh
@@ -4,7 +4,6 @@
 set -euo pipefail # https://stackoverflow.com/a/2871034
 # set -x
 
-npm run build
 npx tsx --tsconfig ./tsconfig.deploy.json buildHeaders.ts
 [[ -f "env.sh" ]] && source env.sh
 npx wrangler pages deploy ./dist --project-name app-ugc --branch main
