e611def20ea6a8e6cf9e6cdff1c208cc5c86138a
Alex <git@pentive.com>
2024-10-17 19:13:52 -0500

    extract image

---
+++ shared/src/image.ts
@@ -0,0 +1,3 @@
+// hacky, but better than my previous solution, which was to parse the value, which was slow(er) and fragile.
+export const imgPlaceholder = '3Iptw8cmfkd/KLrTw+9swHnzxxVhtDCraYLejUh3'
+export const relativeChar = '/'
--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,10 +1,7 @@
 import sqlJson from './sql.json'
 export const initSql = sqlJson
 
-// hacky, but better than my previous solution, which was to parse the value, which was slow(er) and fragile.
-export const imgPlaceholder = '3Iptw8cmfkd/KLrTw+9swHnzxxVhtDCraYLejUh3'
-export const relativeChar = '/'
-
+export * from './image'
 export * from './brand'
 export * from './schema'
 export * from './htmlToText'
