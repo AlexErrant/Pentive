3dc1e4826f1e064f9de878d12d43007528e88bff
Alex <git@pentive.com>
2024-11-12 15:58:55 -0600

    8

--- mkcert.sh
+++ mkcert.sh
@@ -24,8 +24,10 @@ mkcert -key-file peer/.cert/key.pem    -cert-file peer/.cert/cert.pem    peer.pe
 
 mkcert -key-file key.pem               -cert-file cert.pem               user-generated-content-pentive.localhost cwa.pentive.localhost
 
-# if `~/.wrangler/` exists, move `key.pem` and `cert.pem` to it, otherwise move `key.pem` and `cert.pem` to `XDG_CONFIG_HOME` (defaulting to `$HOME/.config`)
-# ref https://github.com/cloudflare/workers-sdk/blob/main/packages/wrangler/src/global-wrangler-config-path.ts#L15
-# Sordid story here https://github.com/cloudflare/workers-sdk/issues/2118#issuecomment-1486184829
-[ -d "${HOME}/.wrangler/" ] && mv key.pem   $HOME/.wrangler/local-cert/key.pem  || mv key.pem  "${XDG_CONFIG_HOME:-$HOME/.config}/.wrangler/local-cert/key.pem"
-[ -d "${HOME}/.wrangler/" ] && mv cert.pem  $HOME/.wrangler/local-cert/cert.pem || mv cert.pem "${XDG_CONFIG_HOME:-$HOME/.config}/.wrangler/local-cert/cert.pem"
+if [[ -z "$CI" ]]; then
+	# if `~/.wrangler/` exists, move `key.pem` and `cert.pem` to it, otherwise move `key.pem` and `cert.pem` to `XDG_CONFIG_HOME` (defaulting to `$HOME/.config`)
+	# ref https://github.com/cloudflare/workers-sdk/blob/main/packages/wrangler/src/global-wrangler-config-path.ts#L15
+	# Sordid story here https://github.com/cloudflare/workers-sdk/issues/2118#issuecomment-1486184829
+	[ -d "${HOME}/.wrangler/" ] && mv key.pem  $HOME/.wrangler/local-cert/key.pem  || mv key.pem  "${XDG_CONFIG_HOME:-$HOME/.config}/.wrangler/local-cert/key.pem"
+	[ -d "${HOME}/.wrangler/" ] && mv cert.pem $HOME/.wrangler/local-cert/cert.pem || mv cert.pem "${XDG_CONFIG_HOME:-$HOME/.config}/.wrangler/local-cert/cert.pem"
+fi
