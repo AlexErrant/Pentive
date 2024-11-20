5290a8772bdcd03ceb36c1576af7aae34e8aff00
Alex <git@pentive.com>
2024-11-19 18:06:03 -0600

    delete containerContext

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -4,17 +4,14 @@ import App from './app'
 import { C } from './topLevelAwait'
 import { ThemeProvider } from 'shared-dom/themeSelector'
 import { DiffModeProvider } from './components/diffModeContext'
-import { ContainerProvider } from './components/containerContext'
 
 render(
 	() => (
-		<ContainerProvider>
-			<DiffModeProvider>
-				<ThemeProvider>
-					<App />
-				</ThemeProvider>
-			</DiffModeProvider>
-		</ContainerProvider>
+		<DiffModeProvider>
+			<ThemeProvider>
+				<App />
+			</ThemeProvider>
+		</DiffModeProvider>
 	),
 	document.getElementById('root') as HTMLElement,
 )
