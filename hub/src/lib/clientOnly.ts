12256a56c399c69b55c7332c201b3dd82bf4d894
Alex <git@pentive.com>
2025-01-13 10:19:22 -0600

    fix bug with multiple appMessengers

--- hub/src/lib/clientOnly.ts
+++ hub/src/lib/clientOnly.ts
@@ -2,11 +2,15 @@
 
 import type { appExpose } from 'app/hubMessenger'
 import * as Comlink from 'comlink'
-import { retryWithTimeout, throwExp } from 'shared/utility'
+import { retryWithTimeout, sleep, throwExp } from 'shared/utility'
 
 let appMessenger: Comlink.Remote<typeof appExpose> | null
+let messengerReady = false
 
 export async function getAppMessenger() {
+	const timeoutMs = 50
+	const failoutMs = 30_000
+	const count = failoutMs / timeoutMs
 	if (appMessenger == null) {
 		const pai = document.getElementById(
 			'pentive-app-iframe',
@@ -15,14 +19,18 @@ export async function getAppMessenger() {
 		appMessenger = Comlink.wrap<typeof appExpose>(
 			Comlink.windowEndpoint(pai.contentWindow),
 		)
-		let messengerReady = false
 		while (!messengerReady) {
-			const timeoutMs = 50
-			const failoutMs = 30_000
-			const count = failoutMs / timeoutMs
 			await retryWithTimeout(appMessenger.ping, count, timeoutMs)
 			messengerReady = true
 		}
+	} else if (!messengerReady) {
+		let i = 0
+		// eslint-disable-next-line no-unmodified-loop-condition
+		while (!messengerReady && i <= count) {
+			i++
+			await sleep(timeoutMs)
+		}
+		if (i === count) throwExp('Cannot connect to appMessenger')
 	}
 	return appMessenger
 }
