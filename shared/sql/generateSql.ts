357b182a98e9a99fedfccc9f917f23e2a72cf05f
Alex <git@pentive.com>
2025-01-17 13:35:48 -0600

    not sure why watch isn't live updating, but I'm too tired to figure out why

--- shared/sql/generateSql.ts
+++ shared/sql/generateSql.ts
@@ -1,23 +1,29 @@
 import * as fs from 'fs'
 import * as chokidar from 'chokidar'
 
+function onChange() {
+	const note = fs.readFileSync('./sql/note.sql').toString()
+	const card = fs.readFileSync('./sql/card.sql').toString()
+	const init = fs.readFileSync('./sql/init.sql').toString()
+
+	const sql = [note, card, init]
+		.join('\n')
+		.split(';')
+		.map((x) => x.trim())
+		.filter((x) => x !== '')
+		.map((x) => x + ';')
+
+	fs.writeFile('./src/sql.json', JSON.stringify(sql, null, 4), (err) => {
+		if (err != null) {
+			console.error(err)
+		}
+	})
+}
+
+onChange()
+
 chokidar
 	.watch(['./sql/init.sql', './sql/note.sql', './sql/card.sql'])
 	.on('change', (event, path) => {
-		const note = fs.readFileSync('./sql/note.sql').toString()
-		const card = fs.readFileSync('./sql/card.sql').toString()
-		const init = fs.readFileSync('./sql/init.sql').toString()
-
-		const sql = [note, card, init]
-			.join('\n')
-			.split(';')
-			.map((x) => x.trim())
-			.filter((x) => x !== '')
-			.map((x) => x + ';')
-
-		fs.writeFile('./src/sql.json', JSON.stringify(sql, null, 4), (err) => {
-			if (err != null) {
-				console.error(err)
-			}
-		})
+		onChange()
 	})
