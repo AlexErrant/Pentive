c0761f398b17996413de38cd79e7677077b6ba01
Alex <git@pentive.com>
2025-01-19 12:24:05 -0600

    remove dead code

--- shared-edge/src/convertBinary.ts
+++ shared-edge/src/convertBinary.ts
@@ -1,27 +1,6 @@
-import { type Base64, type Base64Url, type Hex } from 'shared/brand'
+import { type Hex } from 'shared/brand'
 import { ulidFactory } from 'ulid-workers'
 import { base16, base32crockford } from '@scure/base'
-
-// eslint-disable-next-line @typescript-eslint/naming-convention
-export function toBase64URL_0(base64: Base64): Base64Url {
-	return base64.replaceAll('+', '-').replaceAll('/', '_') as Base64Url
-}
-
-// eslint-disable-next-line @typescript-eslint/naming-convention
-export function fromBase64URL_0(base64url: Base64Url): Base64 {
-	return base64url.replaceAll('-', '+').replaceAll('_', '/') as Base64
-}
-
-export function binary16toBase64URL(base64: Base64): Base64Url {
-	return base64
-		.replaceAll('+', '-')
-		.replaceAll('/', '_')
-		.slice(0, 22) as Base64Url
-}
-
-export function binary16fromBase64URL(base64url: Base64Url): Base64 {
-	return (base64url.replaceAll('-', '+').replaceAll('_', '/') + '==') as Base64
-}
 
 export function ulidAsHex(): Hex {
 	const raw = ulidAsRaw()
