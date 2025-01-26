913012876211a69950fd506f50285c23fa6830f3
Alex <git@pentive.com>
2025-01-26 12:06:31 -0600

    actually don't drop debugger in development

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -54,7 +54,7 @@ export default defineConfig(({ mode }: UserConfig) => {
 	return {
 		define,
 		esbuild: {
-			drop: ['debugger' as const],
+			drop: mode === 'development' ? [] : ['debugger' as const],
 			legalComments: 'none' as const, // we include sourcemaps which have the legal stuff
 		},
 		plugins: [
