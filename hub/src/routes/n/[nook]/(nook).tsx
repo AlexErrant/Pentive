ff799606c334fcdcce20452a7df22beec706a346
Alex <git@pentive.com>
2025-01-29 11:12:54 -0600

    /n/nook uses tanstack table

--- hub/src/routes/n/[nook]/(nook).tsx
+++ hub/src/routes/n/[nook]/(nook).tsx
@@ -18,6 +18,12 @@ import {
 	type RouteSectionProps,
 } from '@solidjs/router'
 import { getNookDetailsCached } from '~/lib/useServer'
+import {
+	flexRender,
+	getCoreRowModel,
+	type ColumnDef,
+	createSolidTable,
+} from '@tanstack/solid-table'
 
 const getPostsCached = query(async (nook: string) => {
 	'use server'
@@ -38,6 +44,8 @@ export const route = {
 	},
 } satisfies RouteDefinition
 
+type Note = Awaited<ReturnType<typeof getNotesCached>>[0]
+
 export default function Nook(props: RouteSectionProps) {
 	const posts = createAsync(
 		async () => await getPostsCached(props.params.nook!),
@@ -45,11 +53,69 @@ export default function Nook(props: RouteSectionProps) {
 	)
 	const notes = createAsync(
 		async () => await getNotesCached(props.params.nook!),
-		{ deferStream: true },
+		{ initialValue: [], deferStream: true },
 	)
 	const nookDetails = createAsync(
 		async () => await getNookDetailsCached(props.params.nook),
 	)
+	const table = createSolidTable({
+		get data() {
+			return notes()
+		},
+		columns: [
+			{
+				header: 'Subscribers',
+				cell: (info) => info.row.original.subscribers,
+			},
+			{
+				header: 'Til',
+				cell: (info) => (
+					<>
+						{info.row.original.til == null ? (
+							''
+						) : (
+							<>
+								Last synced at <RelativeDate date={info.row.original.til} />
+							</>
+						)}
+					</>
+				),
+			},
+			{
+				header: 'Comments',
+				cell: (info) => (
+					<a href={`/n/${props.params.nook}/note/${info.row.original.id}`}>
+						{info.row.original.comments}
+					</a>
+				),
+			},
+			{
+				id: 'preview',
+				cell: (info) => {
+					const localNote = () => remoteToNote(info.row.original.note)
+					const template = () => remoteToTemplate(info.row.original.template)
+					const count = () =>
+						noteOrds.bind(noteOrdsRenderContainer)(localNote(), template())
+							.length - 1
+					return (
+						<>
+							<ResizingIframe
+								i={{
+									tag: 'card',
+									side: 'front',
+									template: template(),
+									card: toSampleCard(0 as Ord),
+									note: localNote(),
+								}}
+							/>
+							<Show when={count() !== 0}>+{count()}</Show>
+						</>
+					)
+				},
+			},
+		] satisfies Array<ColumnDef<Note>>,
+		getCoreRowModel: getCoreRowModel(),
+	})
 	return (
 		<>
 			<ul>
@@ -60,45 +126,66 @@ export default function Nook(props: RouteSectionProps) {
 						</li>
 					)}
 				</For>
-				<For each={notes()}>
-					{(note) => {
-						const localNote = () => remoteToNote(note.note)
-						const template = () => remoteToTemplate(note.template)
-						const count = () =>
-							noteOrds.bind(noteOrdsRenderContainer)(localNote(), template())
-								.length - 1
-						return (
-							<li>
-								<div>
-									{note.til == null ? (
-										''
-									) : (
-										<>
-											Last synced at <RelativeDate date={note.til} />
-										</>
-									)}
-								</div>
-								<div>{note.subscribers} subscribers</div>
-								<div>
-									{/* making this an <A> breaks because maps (e.g. `note.fieldValues`) aren't JSON serializable. Revisit if this issue is ever resolved. https://github.com/TanStack/bling/issues/9 */}
-									<a href={`/n/${props.params.nook}/note/${note.id}`}>
-										{note.comments} comments
-									</a>
-								</div>
-								<ResizingIframe
-									i={{
-										tag: 'card',
-										side: 'front',
-										template: template(),
-										card: toSampleCard(0 as Ord),
-										note: localNote(),
-									}}
-								/>
-								<Show when={count() !== 0}>+{count()}</Show>
-							</li>
-						)
-					}}
-				</For>
+				<table class='w-full table-auto text-left'>
+					<thead>
+						<For each={table.getHeaderGroups()}>
+							{(headerGroup) => (
+								<tr>
+									<For each={headerGroup.headers}>
+										{(header) => (
+											<th>
+												{header.isPlaceholder
+													? null
+													: flexRender(
+															header.column.columnDef.header,
+															header.getContext(),
+														)}
+											</th>
+										)}
+									</For>
+								</tr>
+							)}
+						</For>
+					</thead>
+					<tbody>
+						<For each={table.getRowModel().rows}>
+							{(row) => (
+								<tr>
+									<For each={row.getVisibleCells()}>
+										{(cell) => (
+											<td>
+												{flexRender(
+													cell.column.columnDef.cell,
+													cell.getContext(),
+												)}
+											</td>
+										)}
+									</For>
+								</tr>
+							)}
+						</For>
+					</tbody>
+					<tfoot>
+						<For each={table.getFooterGroups()}>
+							{(footerGroup) => (
+								<tr>
+									<For each={footerGroup.headers}>
+										{(header) => (
+											<th>
+												{header.isPlaceholder
+													? null
+													: flexRender(
+															header.column.columnDef.footer,
+															header.getContext(),
+														)}
+											</th>
+										)}
+									</For>
+								</tr>
+							)}
+						</For>
+					</tfoot>
+				</table>
 			</ul>
 			<Show
 				when={
