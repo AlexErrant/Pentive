7eeaf05824d1ab8fb5970aa828c9381d5696451f
Alex <git@pentive.com>
2025-02-05 20:22:05 -0600

    add eslint.config.js to projects

---
+++ config/eslintconfig.js
@@ -0,0 +1,25 @@
+// @ts-check
+
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import { includeIgnoreFile } from '@eslint/compat'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+/**
+ * @param {string} importMetaUrl
+ */
+export function buildEslintConfig(importMetaUrl) {
+	const projectDirname = path.dirname(fileURLToPath(importMetaUrl))
+	const gitignorePath = path.resolve(__dirname, projectDirname, '.gitignore')
+	return [
+		...tseslint.config(
+			eslint.configs.recommended,
+			tseslint.configs.recommended,
+		),
+		includeIgnoreFile(gitignorePath),
+	]
+}
---
+++ lrpc/eslint.config.js
@@ -0,0 +1,5 @@
+// @ts-check
+
+import { buildEslintConfig } from 'config/eslintconfig.js'
+
+export default buildEslintConfig(import.meta.url)
