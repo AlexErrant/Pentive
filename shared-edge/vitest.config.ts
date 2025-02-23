03bd450fa9535471605a6190641e5d3004961943
Alex <git@pentive.com>
2025-02-22 20:23:28 -0600

    disableConsoleIntercept: true

--- shared-edge/vitest.config.ts
+++ shared-edge/vitest.config.ts
@@ -1,8 +1,13 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
 	test: {
 		exclude: [...configDefaults.exclude, '**/lib/**'],
+		disableConsoleIntercept: true, // "stream" console output during a long test (instead of batching)
+		reporters: process.env.GITHUB_ACTIONS
+			? 'github-actions' //
+			: 'basic', // https://github.com/vitest-dev/vscode/discussions/117#discussioncomment-8882237
 		chaiConfig: {
 			truncateThreshold: 0,
 		},
